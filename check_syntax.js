5d66f653d1a058e84396c1398af1249983f34b43
PaphangkornGG <paphangkorn.twrp@gmail.com>
2026-06-06 10:54:06 +0700

    Fix bento-app.jsx syntax error and add ErrorBoundary safely

--- check_syntax.js
+++ check_syntax.js
@@ -1,23 +1,12 @@
 const fs = require('fs');
-const https = require('https');
-https.get('https://unpkg.com/@babel/standalone@7.29.0/babel.min.js', (res) => {
-  let body = '';
-  res.on('data', d => body += d);
-  res.on('end', () => {
-    try {
-      const sandbox = { window: {}, setTimeout, clearTimeout, console };
-      require('vm').createContext(sandbox);
-      require('vm').runInContext(body, sandbox);
-      const Babel = sandbox.Babel || sandbox.window.Babel;
-      const files = ['data.jsx', 'history.jsx', 'pages.jsx', 'bento-app.jsx'];
-      for (const f of files) {
-        try {
-          Babel.transform(fs.readFileSync(f, 'utf8'), { presets: ['react'], filename: f });
-          console.log(f, 'OK');
-        } catch (e) {
-          console.error(f, 'ERROR:', e.message);
-        }
-      }
-    } catch(e) { console.error('Babel load error', e); }
-  });
-});
+const acorn = require('acorn');
+const jsx = require('acorn-jsx');
+const Parser = acorn.Parser.extend(jsx());
+
+const code = fs.readFileSync(process.argv[2], 'utf8');
+try {
+  Parser.parse(code, { ecmaVersion: 2020, sourceType: 'module' });
+  console.log("Syntax is OK!");
+} catch (e) {
+  console.error("Syntax Error:", e);
+}
